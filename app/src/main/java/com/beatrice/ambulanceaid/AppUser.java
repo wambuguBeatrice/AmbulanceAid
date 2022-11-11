8f5208f253146b81e91e114aa7b8b09ce486d516
wambuguBeatrice <78013094+wambuguBeatrice@users.noreply.github.com>
2022-11-11 09:46:10 +0300

    Initial commit

---
+++ app/src/main/java/com/beatrice/ambulanceaid/AppUser.java
@@ -0,0 +1,6 @@
+package com.beatrice.ambulanceaid;
+
+public class AppUser {
+    public AppUser(String username, String useremail, String usermobilenumber) {
+    }
+}
---
+++ app/src/main/java/com/beatrice/ambulanceaid/SplashActivity.java
@@ -0,0 +1,25 @@
+package com.beatrice.ambulanceaid;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //overridePendingTransition(R.anim.abc_fade_in,R.anim.abc_fade_out);
+        setContentView(R.layout.activity_splash);
+        //hiding the action bar on the splashscreen.
+        getSupportActionBar().hide();
+        new android.os.Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                finish();
+                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+            }
+        },4000);
+    }
+}
